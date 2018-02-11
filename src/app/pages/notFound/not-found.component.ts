7077b71d0cbcb28f94eea7d6275f0c598d1da6e6
beloha <belohadk97@gmail.com>
2018-02-11 20:07:34 +0200

    page not found sample style and text

--- src/app/pages/notFound/not-found.component.ts
+++ src/app/pages/notFound/not-found.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'not-found',
     templateUrl: './not-found.template.html',
-    styles: ['./not-found.style.scss']
+    styleUrls: ['./not-found.style.scss']
 })
 class NotFoundComponent {
 

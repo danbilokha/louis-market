28bff1513518ec1db974dc11f7d37d92c4627eef
beloha <belohadk97@gmail.com>
2018-02-12 23:52:29 +0200

    worked on the admin routing

--- src/app/specific-rights/admin/settings/admin.routing.ts
+++ src/app/specific-rights/admin/settings/admin.routing.ts
@@ -4,14 +4,22 @@ import {Routes, RouterModule} from '@angular/router';
 import {AdminComponent} from '../admin.component';
 import {AdminIndexComponent} from '../pages/index/index.component';
 import {AdminStuffComponent} from '../pages/stuff/stuff.component';
+import {AdminStuffCreateComponent} from '../pages/stuff/create/create.component';
+import {AdminStuffDeleteComponent} from '../pages/stuff/delete/delete.component';
+import {AdminStuffUpdateComponent} from '../pages/stuff/update/update.component';
 
 const routes: Routes =[
     { path: '',             component: AdminComponent,
         children: [
-            { path: 'home',          component: AdminIndexComponent },
-            { path: 'stuff',         component: AdminStuffComponent },
-            { path: '',              component: AdminComponent, pathMatch: 'full' },
-        ]},
+            { path: 'main',          component: AdminIndexComponent },
+            { path: 'stuff',         component: AdminStuffComponent,
+                children: [
+                    { path: 'create',          component: AdminStuffCreateComponent },
+                    { path: 'delete',          component: AdminStuffDeleteComponent },
+                    { path: 'update',          component: AdminStuffUpdateComponent },
+                ] },
+        ] },
+    // { path: '',              component: AdminComponent, pathMatch: 'full' },
     { path: "**", redirectTo: '' }
 ];
 

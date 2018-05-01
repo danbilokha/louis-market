351511083b1b7f1a07a21e84d0b402d7c3028212
beloha <belohadk97@gmail.com>
2018-05-01 21:15:15 +0300

    --fixed data collision at watch page

--- src/app/pages/watch/watch.component.ts
+++ src/app/pages/watch/watch.component.ts
@@ -1,23 +1,49 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-
 import {Watch} from '@common/dictionaries/watch.dictionary';
 import {LouisImage} from '@common/dictionaries/Image.dictionary';
 import {DEFAULT_WATCH_IMAGE} from '@settings/constants';
+import {WatchService} from './watch.service';
+import {findWatchByName} from './watch.calculation';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+import 'rxjs/add/operator/skip';
+import 'rxjs/add/operator/catch';
 
 @Component({
     selector: 'louis-watch-page',
     templateUrl: './watch.template.html',
     styleUrls: ['./watch.style.scss']
 })
-class WatchPageComponent implements OnInit {
+class WatchPageComponent implements OnInit, OnDestroy {
 
-    public watch: Watch = {} as any;
-
+    public watch: Watch;
     public mainImage: LouisImage;
     public priceMap: object;
+    private watchSubscription: Subscription = this.watchService
+        .getWatches()
+        .skip(1)
+        .withLatestFrom(this.route.paramMap,
+            (watches, param) => {
+                if(param.has('name')) {
+                    const watchName = param.get('name');
+                    return findWatchByName(watchName)(watches);
+                } else {
+                    Observable.throw('Not such watch');
+                }
+            })
+        .catch(error => {
+            console.error(`Not found watch. Error: ${error.message}`);
+            return Observable.of(undefined);
+        })
+        .switchMap(v => Observable.of(v[0]))
+        .subscribe(watch => {
+            this.mainImage = this.getMainImage(watch.images);
+            this.watch = watch;
+        });
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute,
+                private watchService: WatchService) {
     }
 
     ngOnInit() {
@@ -37,6 +63,10 @@ class WatchPageComponent implements OnInit {
             discount: this.watch.discount,
             toFixed: 2
         };
+    }
+
+    ngOnDestroy() {
+        this.watchSubscription.unsubscribe();
     }
 
     private getMainImage(images: Array<LouisImage>): LouisImage {
